4dc7f55d93f31c543c97e838b31f950d7770dbf2
Temur <shernazarovtemurcom001@gmail.com>
2023-12-21 10:40:14 +0500

    navbar shrink is started

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ function toggleBacktop() {
     backtop.style.bottom = "-80px";
   }
 }
+// NAVBAR SHRINK
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "7px 5px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "20px 5px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
