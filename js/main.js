9b893722a44431ba83490fc5b718bc2e7f7c1fcc
Temur <shernazarovtemurcom001@gmail.com>
2023-12-21 11:28:16 +0500

    darkmode is started

--- js/main.js
+++ js/main.js
@@ -29,3 +29,15 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+// DARK LIGHT
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "../images/light.svg";
+  } else {
+    this.firstElementChild.src = "../images/dark.svg";
+  }
+  document.body.classList.toggle("light");
+});
